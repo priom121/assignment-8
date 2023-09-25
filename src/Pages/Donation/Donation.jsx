3b2877e5b92a7ddae8b469ec32c0cf09f382d384
Ypriom121 <pritomgupta121@gmail.com>
2023-09-26 00:01:20 +0600

    some condition added for show more button

--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -1,16 +1,30 @@
 import { useEffect, useState } from "react";
+import DonateCard from "./DonateCard";
 
 
 const Donation = () => {
-     const [donation ,setDonation] = useState();
+     const [donation ,setDonation] = useState([]);
+     const [show ,setShow] =useState(false)
      useEffect(()=>{
           const donates = JSON.parse(localStorage.getItem('donation'));
-          setDonation(donates)
+               setDonation(donates)
      },[])
-     console.log(donation);
+     // console.log(donation);
+     // console.log(show);
  return (
- <div>
-                                                                                       
+ <div  >
+   <div className="grid grid-cols-2 gap-5">
+   {
+     show ? donation.map(donate=><DonateCard key={donate.id} donate={donate}></DonateCard>) 
+     : donation.slice(0,4).map(donate=><DonateCard key={donate.id} donate={donate}></DonateCard>)
+
+          // donation.map(donate=><DonateCard key={donate.id} donate={donate}></DonateCard>)
+     } 
+   </div>
+ 
+     <button onClick={()=>setShow(!show)} className= "px-3 py-3 block mx-auto rounded mt-5 bg-green-500">
+          Show more</button>
+                                                                                
  </div>
  );
 };
