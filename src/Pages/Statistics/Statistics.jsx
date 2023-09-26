c4037bd1887c1e74f936f1a573273fa535a6bf48
Ypriom121 <pritomgupta121@gmail.com>
2023-09-26 23:08:58 +0600

    pie chart calculation and show the display done

--- src/Pages/Statistics/Statistics.jsx
+++ src/Pages/Statistics/Statistics.jsx
@@ -1,11 +1,27 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import Chart from "react-google-charts";
 
 
 const Statistics = () => {
- return (
-  <div>
-      Statistics                                                                                    
-</div>
-);
+    const [pie , setPie] =useState([]);
+    useEffect(()=>{
+        const donates = JSON.parse(localStorage.getItem('donation'));
+        setPie(donates)
+    },[])
+    const totalDonation = 12 - pie.length ;
+    const yourDonation = pie.length
+    return (
+        <div className="flex  justify-center items-center h-[80vh]">
+            <Chart  chartType="PieChart" width={"100%"}
+      height={"500px"} data={[
+        ["donate" , "value"],
+        ["Total Donation" , totalDonation],
+        ["Your Donation" , yourDonation]
+      ]} >
+            </Chart>
+        </div>
+    );
 };
 
 export default Statistics;
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -24,7 +24,8 @@ const myCreateRoute = createBrowserRouter ([
             } ,
            {
               path:'/statistics',
-              element:<Statistics></Statistics>                
+              element:<Statistics></Statistics>,
+                             
            },
            {
             path:'/allCards/:id',
